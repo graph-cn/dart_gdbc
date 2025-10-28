dc44e393f7a1b9202288ac862fd2035466c72370
CorvusYe <CorvusY@foxmail.com>
2025-10-28 09:52:39 +0800

    chore: 负责实现的子模块拆分到其他文件夹

--- impls/pool/dart_gdbc_pool/bin/dart_gdbc_pool.dart
+++
@@ -1,3 +0,0 @@
-// import 'package:dart_gdbc_pool/dart_gdbc_pool.dart' as dart_gdbc_pool;
-
-void main(List<String> arguments) {}
--- impls/pool/dart_gdbc_pool/test/dart_gdbc_pool_test.dart
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2023- All dart_gdbc_pool authors. All rights reserved.
-//
-// This source code is licensed under Apache 2.0 License.
-
-// import 'package:dart_gdbc_pool/dart_gdbc_pool.dart';
-// import 'package:test/test.dart';
